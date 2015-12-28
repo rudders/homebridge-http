95118db155f59cb824a86f9c3343545ecb080c73
Adrian Rudman <a.rudman@binarylight.com.au>
2015-12-29 03:51:39 +1100

    Update with PR from NovaGL

--- README.md
+++ README.md
@@ -1,37 +1,34 @@
-# homebridge-http
+# homebridge-httpstatus
 
-Supports https devices on HomeBridge Platform
+Supports https devices on the HomeBridge Platform and provides a readable callback for getting the "On" and brightness level characteristics to Homekit.
 
 # Installation
 
 1. Install homebridge using: npm install -g homebridge
-2. Install this plugin using: npm install -g homebridge-http
+2. Install this plugin by cloning this repo.
 3. Update your configuration file. See sample-config.json in this repository for a sample. 
 
 # Configuration
 
-if your http method for brightness is different to switching then add "http_brightness_method" to your config to set this - it will default to http_method if not present.
+The configuration for this plugin is the same as [homebridge-http](https://github.com/rudders/homebridge-http) but includes an additional method to read the power state of the device and the brightness level. Specify the `status_url` in your config.json that returns the status of the device as an integer (0 = off, 1 = on). Specify the `brightnesslvl_url` to return the current brighness level as an integer.
 
 Configuration sample:
 
  ```
-"accessories": [
-        {
-            "accessory": "Http",
-            "name": "Kitchen Lamp",
-            "on_url": "https://192.168.1.22:3030/devices/23222/on",
-            "on_body": "{\"state\":\"On\"}",
-            "off_url": "https://192.168.1.22:3030/devices/23222/off",
-            "off_body": "{\"state\":\"Off\"}",
-            "brightness_url": "https://192.168.1.22:3030/devices/23222/brightness/%b",
-			"username": "",
-			"password": "",
-			"sendimmediately": "",
-            "http_method": "POST",
-            "http_brightness_method": "POST",       
-			"service": "Switch",
-			"brightnessHandling": "no"
-        }
+"accessories": [ 
+	{
+		"accessory": "HttpStatus",
+		"name": "Alfresco Lamp",
+		"switchHandling": "yes",
+				"http_method": "GET",
+				"on_url":      "http://localhost/controller/1700/ON",
+				"off_url":     "http://localhost/controller/1700/OFF",
+				"status_url":  "http://localhost/status/100059",
+		"service": "Light",
+				"brightnessHandling": "yes",
+				"brightness_url":     "http://localhost/controller/1707/%b",
+				"brightnesslvl_url":  "http://localhost/status/100054"			
+									    
+       } 
     ]
-
 ```
--- index.js
+++ index.js
@@ -4,131 +4,202 @@ var request = require("request");
 module.exports = function(homebridge){
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
-  homebridge.registerAccessory("homebridge-http", "Http", HttpAccessory);
+  homebridge.registerAccessory("homebridge-httpstatus", "HttpStatus", HttpStatusAccessory);
 }
 
 
-function HttpAccessory(log, config) {
-	this.log = log;
+function HttpStatusAccessory(log, config) {
+  this.log = log;
 
-	// url info
-	this.on_url    = config["on_url"];
-	this.on_body   = config["on_body"];
-	this.off_url   = config["off_url"];
-	this.off_body  = config["off_body"];
-	this.brightness_url = config["brightness_url"];
-	this.http_method = config["http_method"];
-	this.http_brightness_method = config["http_brightness_method"] || this.http_method;
-	this.username = config["username"];
-	this.password = config["password"];
-	this.sendimmediately = config["sendimmediately"];
-	this.service = config["service"] || "Switch";
-	this.name = config["name"];
-	this.brightnessHandling = config["brightnessHandling"] || "no";
+  // url info
+  this.on_url                 = config["on_url"];
+  this.on_body                = config["on_body"];
+  this.off_url                = config["off_url"];
+  this.off_body               = config["off_body"];
+  this.status_url             = config["status_url"];
+  this.brightness_url         = config["brightness_url"];
+  this.brightnesslvl_url      = config["brightnesslvl_url"];
+  this.http_method            = config["http_method"] 	  	 || "GET";;
+  this.http_brightness_method = config["http_brightness_method"] || this.http_method;
+  this.username               = config["username"] 	  	 || "";
+  this.password               = config["password"] 	  	 || "";
+  this.sendimmediately        = config["sendimmediately"] 	 || "";
+  this.service                = config["service"] 	  	 || "Switch";
+  this.name                   = config["name"];
+  this.brightnessHandling     = config["brightnessHandling"] 	 || "no";
+  this.switchHandling = config["switchHandling"] 		 || "no";
 }
 
-HttpAccessory.prototype = {
+HttpStatusAccessory.prototype = {
 
-	httpRequest: function(url, body, method, username, password, sendimmediately, callback) {
-		request({
-				url: url,
-				body: body,
-				method: method,
-				rejectUnauthorized: false,
-				auth: {
-					user: username,
-					pass: password,
-					sendImmediately: sendimmediately
-				}
-			},
-			function(error, response, body) {
-				callback(error, response, body)
-			})
-	},
+  httpRequest: function(url, body, method, username, password, sendimmediately, callback) {
+    request({
+      url: url,
+      body: body,
+      method: method,
+      auth: {
+        user: username,
+        pass: password,
+        sendImmediately: sendimmediately
+      }
+    },
+    function(error, response, body) {
+      callback(error, response, body)
+    })
+  },
 
-	setPowerState: function(powerOn, callback) {
-		var url;
-		var body;
+  setPowerState: function(powerOn, callback) {
+    var url;
+    var body;
 
-		if (powerOn) {
-			url = this.on_url;
-			body = this.on_body;
-			this.log("Setting power state to on");
-		} else {
-			url = this.off_url;
-			body = this.off_body;
-			this.log("Setting power state to off");
-		}
+    if (!this.on_url || !this.off_url) {
+    	    this.log.warn("Ignoring request; No power url defined.");
+	    callback(new Error("No power url defined."));
+	    return;
+    }
 
-		this.httpRequest(url, body, this.http_method, this.username, this.password, this.sendimmediately, function(error, response, responseBody) {
-			if (error) {
-				this.log('HTTP power function failed: %s', error.message);
-				callback(error);
-			} else {
-				this.log('HTTP power function succeeded!');
-				this.log(response);
-				this.log(responseBody);
+    if (powerOn) {
+      url = this.on_url;
+      body = this.on_body;
+      this.log("Setting power state to on");
+    } else {
+      url = this.off_url;
+      body = this.off_body;
+      this.log("Setting power state to off");
+    }
+
+    this.httpRequest(url, body, this.http_method, this.username, this.password, this.sendimmediately, function(error, response, responseBody) {
+      if (error) {
+        this.log('HTTP set power function failed: %s', error.message);
+        callback(error);
+      } else {
+        this.log('HTTP set power function succeeded!');
+        callback();
+      }
+    }.bind(this));
+  },
+  
+  getPowerState: function(callback) {
+    if (!this.status_url) {
+    	    this.log.warn("Ignoring request; No status url defined.");
+	    callback(new Error("No status url defined."));
+	    return;
+    }
+    
+    var url = this.status_url;
+    this.log("Getting power state");
+
+    this.httpRequest(url, "", "GET", this.username, this.password, this.sendimmediately, function(error, response, responseBody) {
+      if (error) {
+        this.log('HTTP get power function failed: %s', error.message);
+        callback(error);
+      } else {
+        var binaryState = parseInt(responseBody);
+        var powerOn = binaryState > 0;
+        this.log("Power state is currently %s", binaryState);
+        callback(null, powerOn);
+      }
+    }.bind(this));
+  },
+
+getBrightness: function(callback) {
+	if (!this.brightnesslvl_url) {
+    	    this.log.warn("Ignoring request; No brightness level url defined.");
+	    callback(new Error("No brightness level url defined."));
+	    return;
+	 }		
+		var url = this.brightnesslvl_url;
+		this.log("Getting Brightness level");
+
+		this.httpRequest(url, "", "GET", this.username, this.password, this.sendimmediately, function(error, response, responseBody) {
+		  if (error) {
+			this.log('HTTP get brightness function failed: %s', error.message);
+			callback(error);
+		  } else {			
+		    var binaryState = parseInt(responseBody);
+			var level = binaryState;
+			this.log("brightness state is currently %s", binaryState);
+			callback(null, level);
+		  }
+		}.bind(this));
+	  },
+
+  setBrightness: function(level, callback) {
 	
-				callback();
-			}
-		}.bind(this));
-	},
+	if (!this.brightness_url) {
+    	    this.log.warn("Ignoring request; No brightness url defined.");
+	    callback(new Error("No brightness url defined."));
+	    return;
+	 }    
 
-	setBrightness: function(level, callback) {
-		var url = this.brightness_url.replace("%b", level)
+    var url = this.brightness_url.replace("%b", level)
 
-		this.log("Setting brightness to %s", level);
+    this.log("Setting brightness to %s", level);
 
-		this.httpRequest(url, "", this.http_brightness_method, this.username, this.password, this.sendimmediately, function(error, response, body) {
-			if (error) {
-				this.log('HTTP brightness function failed: %s', error);
-				callback(error);
-			} else {
-				this.log('HTTP brightness function succeeded!');
-				callback();
-			}
-		}.bind(this));
-	},
+    this.httpRequest(url, "", this.http_brightness_method, this.username, this.password, this.sendimmediately, function(error, response, body) {
+      if (error) {
+        this.log('HTTP brightness function failed: %s', error);
+        callback(error);
+      } else {
+        this.log('HTTP brightness function succeeded!');
+        callback();
+      }
+    }.bind(this));
+  },
 
-	identify: function(callback) {
-		this.log("Identify requested!");
-		callback(); // success
-	},
+  identify: function(callback) {
+    this.log("Identify requested!");
+    callback(); // success
+  },
 
-	getServices: function() {
+  getServices: function() {
 
-		// you can OPTIONALLY create an information service if you wish to override
-		// the default values for things like serial number, model, etc.
-		var informationService = new Service.AccessoryInformation();
+    // you can OPTIONALLY create an information service if you wish to override
+    // the default values for things like serial number, model, etc.
+    var informationService = new Service.AccessoryInformation();
 
-		informationService
-			.setCharacteristic(Characteristic.Manufacturer, "HTTP Manufacturer")
-			.setCharacteristic(Characteristic.Model, "HTTP Model")
-			.setCharacteristic(Characteristic.SerialNumber, "HTTP Serial Number");
+    informationService
+    .setCharacteristic(Characteristic.Manufacturer, "HTTP Manufacturer")
+    .setCharacteristic(Characteristic.Model, "HTTP Model")
+    .setCharacteristic(Characteristic.SerialNumber, "HTTP Serial Number");
 
-		if (this.service == "Switch") {
-			var switchService = new Service.Switch(this.name);
+    if (this.service == "Switch") {
+      var switchService = new Service.Switch(this.name);
 
+      if (this.switchHandling == "yes") {
 			switchService
-				.getCharacteristic(Characteristic.On)
-				.on('set', this.setPowerState.bind(this));
+			.getCharacteristic(Characteristic.On)
+			.on('get', this.getPowerState.bind(this))
+			.on('set', this.setPowerState.bind(this));
+		  } else {
+			switchService
+			.getCharacteristic(Characteristic.On)	
+			.on('set', this.setPowerState.bind(this));
+		  }
+      return [switchService];
+    } else if (this.service == "Light") {
+      var lightbulbService = new Service.Lightbulb(this.name);
 
-			return [switchService];
-		} else if (this.service == "Light") {
-			var lightbulbService = new Service.Lightbulb(this.name);
-
+	if (this.switchHandling == "yes") {
 			lightbulbService
-				.getCharacteristic(Characteristic.On)
-				.on('set', this.setPowerState.bind(this));
+			.getCharacteristic(Characteristic.On)
+			.on('get', this.getPowerState.bind(this))
+			.on('set', this.setPowerState.bind(this));
+		  } else {
+			lightbulbService
+			.getCharacteristic(Characteristic.On)	
+			.on('set', this.setPowerState.bind(this));
+		  }
 
-			if (this.brightnessHandling == "yes") {
+      if (this.brightnessHandling == "yes") {
 
-				lightbulbService
-					.addCharacteristic(new Characteristic.Brightness())
-					.on('set', this.setBrightness.bind(this));
-			}
+        lightbulbService
+        .addCharacteristic(new Characteristic.Brightness())
+        .on('get', this.getBrightness.bind(this))
+        .on('set', this.setBrightness.bind(this));
+      }
 
-			return [informationService, lightbulbService];
-		}
-	}
+      return [informationService, lightbulbService];
+    }
+  }
 };
